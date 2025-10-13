57103972787c1fcc7c8ba81bf0ecd291f0b01b29
kobayashi5289 <jimonjitou5884@gmail.com>
2025-10-13 19:01:56 +0900

    push

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController _characterController;
+    [SerializeField] private AudioSource _audioSource;
     [SerializeField] private InputActionReference _move;
     [SerializeField] private InputActionReference _turn;
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _mouseSensitive = 1f;
+    [SerializeField] private int _micNo = 2;
 
     private void Awake()
     {
         _move.action.Enable();
         _turn.action.Enable();
+
+        string micName = Microphone.devices[_micNo];
+        _audioSource.clip = Microphone.Start(micName, true, 10, 44100);
+        while (!(Microphone.GetPosition(micName) > 0))
+        {
+
+        }
     }
 
     private void Update()
